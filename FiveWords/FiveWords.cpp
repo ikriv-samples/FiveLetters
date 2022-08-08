3d1bbe3bcc7340e5bcfcdc755c2ddd2fd87ac7a0
Ivan Krivyakov <ivan@ikriv.com>
2022-08-08 12:02:23 -0400

    Better implementation of 5 word search that seems to actually work

--- FiveWords/FiveWords.cpp
+++ FiveWords/FiveWords.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <unordered_map>
 #include <algorithm>
-#include <array>
 #include <limits.h>
 
 using namespace std;
@@ -23,7 +23,7 @@ void read_input(istream& input, vector<string>& words) {
 	}
 }
 
-int word_to_int(string& word) {
+int word_to_mask(string& word) {
 	if (word.length() != EXPECTED_LENGTH) return 0;
 	int result = 0;
 	for (auto c : word) {
@@ -39,101 +39,131 @@ int word_to_int(string& word) {
 	return result;
 }
 
-void build_words_map(vector<string>& words, multimap<int, string>& words_map) {
-	for (auto word : words) {
-		if (word.length() == 0) continue;
-		words_map.insert(make_pair(word_to_int(word), word));
+typedef unordered_map<int, vector<string>> word_map;
+
+template<typename T>
+void insert(unordered_map<int, vector<T>>& map, int key, T const& value) {
+	auto it = map.find(key);
+	if (it != map.end()) {
+		it->second.push_back(value);
+	}
+	else {
+		map.insert({ key, {value} });
 	}
 }
 
-void print_invalid_words(multimap<int, string> const& words_map) {
-	if (words_map.count(0) == 0) return;
+void build_words_map(vector<string>& words, word_map& mask_to_words) {
+	for (auto word : words) {
+		if (word.length() == 0) continue;
+		insert(mask_to_words, word_to_mask(word), word);
+	}
+}
+
+void print_invalid_words(word_map const& map) {
+	auto it = map.find(0);
+	if (it == map.end()) return;
 
 	cerr << "Invalid words: " << endl;
-	auto bad_words = words_map.equal_range(0);
-	for_each(bad_words.first, bad_words.second, [](pair<int, string> const& entry) {
-		cerr << entry.second << endl;
-		});
+	for (auto word : it->second) {
+		cerr << word << endl;
+	}
 	cerr << "End invalid words" << endl;
 }
 
-typedef __int64 int64;
-
-void get_sorted_keys(multimap<int, string> const& words_map, vector<int>& keys) {
-	for (auto entry : words_map) {
+template<typename T>
+void get_unique_keys(unordered_map<int, T> const& map, vector<int>& keys) {
+	if (map.size() == 0) return;
+	for (auto entry : map) {
 		keys.push_back(entry.first);
 	}
-	sort(keys.begin(), keys.end());
 }
-typedef vector<array<int, NUM_WORDS>> group_vector;
 
-void keys_to_groups(vector<int> const& keys, group_vector& groups) {
-	groups.reserve(keys.size());
-	for (auto key : keys) {
-		array<int,NUM_WORDS> group = { key };
-		groups.push_back(group);
+typedef pair<int, int> mask_pair;
+typedef unordered_map<int, vector<mask_pair>> mask_to_pair_map;
+
+void nand(mask_to_pair_map const& input_map, vector<int> const& word_masks, mask_to_pair_map& output_map) {
+	for (auto entry : input_map) {
+		const int input_mask = entry.first;
+		for (auto word_mask : word_masks) {
+			if ((input_mask & word_mask) == 0) {
+				const int output_mask = input_mask | word_mask;
+				insert(output_map, output_mask, make_pair(word_mask, input_mask));
+			}
+		}
 	}
 }
 
-void find_groups(group_vector const& input_groups, int input_group_size, vector<int> const& keys, group_vector& output_groups) {
-	for (array<int,5> const& input_group : input_groups) {
-		int last = input_group[input_group_size - 1];
-		for (auto key : keys) {
-			if (key <= last) continue;
-			bool good_key = true;
-			for (int i = 0; i < input_group_size; ++i) {
-				if ((key & input_group[i]) != 0) {
-					good_key = false;
-					break;
-				}
+void print_words_impl(mask_to_pair_map* pair_maps, word_map const& mask_to_words, vector<string>& prefix, int mask) {
+	const int level = NUM_WORDS - int(prefix.size());
+	if (level == 0) {
+		// print all the words in the vector
+		bool is_first = true;
+		for (auto word : prefix) {
+			if (is_first) {
+				is_first = false;
 			}
-			if (good_key) {
-				array<int, NUM_WORDS> new_group;
-				for (int i = 0; i < input_group_size; ++i) {
-					new_group[i] = input_group[i];
-				}
-				new_group[input_group_size] = key;
-				output_groups.push_back(new_group);
+			else {
+				cout << " ";
+			}
+			cout << word;
+		}
+		cout << endl;
+	}
+	else {
+		auto it = pair_maps[level].find(mask);
+		if (it == pair_maps[level].end()) {
+			throw logic_error("Mask not found in the pair map");
+		}
+
+		for (auto mask_pair : it->second) {
+			auto word_mask = mask_pair.first;
+			auto suffix_mask = mask_pair.second;
+
+			auto word_it = mask_to_words.find(word_mask);
+			if (word_it == mask_to_words.end()) {
+				throw logic_error("Mask not found in the word map");
+			}
+			auto const last_word_of_prefix = (prefix.size() > 0) ? prefix[prefix.size() - 1] : "";
+			for (auto word : word_it->second) {
+				if (word <= last_word_of_prefix) continue;
+				prefix.push_back(word);
+				print_words_impl(pair_maps, mask_to_words, prefix, suffix_mask);
+				prefix.pop_back();
 			}
 		}
+	}
+}
+
+void print_words(mask_to_pair_map* pair_maps, word_map const& mask_to_words) {
+	vector<string> prefix;
+	for (auto entry : pair_maps[NUM_WORDS]) {
+		print_words_impl(pair_maps, mask_to_words, prefix, entry.first);
 	}
 }
 
 int main()
 {
-	auto int_bits = sizeof(int64) * CHAR_BIT;
-	if (int_bits < 64) {
-		cerr << "At least 64-bit integers are required, " << int_bits << " bits detected" << endl;
-		return 1;
-	}
 	vector<string> words;
 	read_input(cin, words);
 	cerr << words.size() << " words" << endl;
 
-	multimap<int, string> words_map;
-	build_words_map(words, words_map);
-
-	print_invalid_words(words_map);
+	word_map mask_to_words;
+	build_words_map(words, mask_to_words);
+	print_invalid_words(mask_to_words);
+	mask_to_words.erase(0);
 
 	vector<int> keys;
-	get_sorted_keys(words_map, keys);
+	get_unique_keys(mask_to_words, keys);
+	cerr << keys.size() << " unique keys" << endl;
 
-	group_vector groups;
-	keys_to_groups(keys, groups);
+	mask_to_pair_map pair_maps[NUM_WORDS+1];
+	pair_maps[0].insert({ 0,{{0,0}} });
+	for (int i = 1; i <= NUM_WORDS; ++i) {
+		nand(pair_maps[i-1], keys, pair_maps[i]);
+		cerr << pair_maps[i].size() << " groups of size " << i << endl;
+	}
 
-	try {
-		for (int group_size = 1; group_size < NUM_WORDS; ++group_size) {
-			group_vector new_groups;
-			find_groups(groups, group_size, keys, new_groups);
-			cerr << new_groups.size() << " groups of size " << group_size + 1 << endl;
-			groups = new_groups;
-		}
-	}
-	catch (exception const& e)
-	{
-		cerr << "Error: " << e.what();
-		return 2;
-	}
+	print_words(pair_maps, mask_to_words);
 
 	return 0;
 }
